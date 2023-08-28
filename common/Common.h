d8f425c5a450db7339ebbbccdbf9381bc5cfa28b
liushuai <770722922@qq.com>
2023-08-28 21:00:37 +0800

    feat: update asio_net

--- common/Common.h
+++ common/Common.h
@@ -8,44 +8,29 @@ namespace cpu_monitor {
 
 static const uint32_t MessageMaxByteSize = 1024 * 1024 * 10;
 
-}  // namespace cpu_monitor
-
-namespace cpu_monitor {
 namespace msg {
+
 struct CpuInfo {
   std::string name{};
   float usage = 0.0f;
   uint64_t timestamps = 0;
 };
-}  // namespace msg
-}  // namespace cpu_monitor
-RPC_CORE_DEFINE_TYPE(cpu_monitor::msg::CpuInfo, name, usage, timestamps);
+RPC_CORE_DEFINE_TYPE(CpuInfo, name, usage, timestamps);
 
-namespace cpu_monitor {
-namespace msg {
 struct CpuMsg {
   CpuInfo ave{};
   std::vector<CpuInfo> cores{};
   uint64_t timestamps = 0;
 };
-}  // namespace msg
-}  // namespace cpu_monitor
-RPC_CORE_DEFINE_TYPE(cpu_monitor::msg::CpuMsg, ave, cores, timestamps);
+RPC_CORE_DEFINE_TYPE(CpuMsg, ave, cores, timestamps);
 
-namespace cpu_monitor {
-namespace msg {
 struct ThreadInfo {
   std::string name{};
   uint64_t id = 0;
   float usage = 0.0f;
   uint64_t timestamps = 0;
 };
-}  // namespace msg
-}  // namespace cpu_monitor
-RPC_CORE_DEFINE_TYPE(cpu_monitor::msg::ThreadInfo, name, id, usage, timestamps);
-
-namespace cpu_monitor {
-namespace msg {
+RPC_CORE_DEFINE_TYPE(ThreadInfo, name, id, usage, timestamps);
 
 struct MemInfo {
   uint64_t peak = 0;
@@ -54,13 +39,7 @@ struct MemInfo {
   uint64_t rss = 0;
   uint64_t timestamps = 0;
 };
-
-}  // namespace msg
-}  // namespace cpu_monitor
-
-RPC_CORE_DEFINE_TYPE(cpu_monitor::msg::MemInfo, peak, size, hwm, rss, timestamps);
-namespace cpu_monitor {
-namespace msg {
+RPC_CORE_DEFINE_TYPE(MemInfo, peak, size, hwm, rss, timestamps);
 
 struct ProcessInfo {
   uint64_t id = 0;
@@ -68,16 +47,13 @@ struct ProcessInfo {
   std::vector<ThreadInfo> thread_infos{};
   MemInfo mem_info{};
 };
-}  // namespace msg
-}  // namespace cpu_monitor
-RPC_CORE_DEFINE_TYPE(cpu_monitor::msg::ProcessInfo, id, name, thread_infos, mem_info);
+RPC_CORE_DEFINE_TYPE(ProcessInfo, id, name, thread_infos, mem_info);
 
-namespace cpu_monitor {
-namespace msg {
 struct ProcessMsg {
   std::vector<ProcessInfo> infos{};
   uint64_t timestamps = 0;
 };
+RPC_CORE_DEFINE_TYPE(ProcessMsg, infos, timestamps);
+
 }  // namespace msg
 }  // namespace cpu_monitor
-RPC_CORE_DEFINE_TYPE(cpu_monitor::msg::ProcessMsg, infos, timestamps);
