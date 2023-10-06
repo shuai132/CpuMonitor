86c4377c1433812af1627fb2dcce49ba0eb0239c
liushuai <770722922@qq.com>
2023-10-06 14:09:06 +0800

    feat: basic echarts demo

--- ui-tauri/cpu_monitor/src/App.vue
+++ ui-tauri/cpu_monitor/src/App.vue
@@ -1,52 +1,90 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import Greet from "./components/Greet.vue";
+import {ref} from "vue";
+import {onMounted, onUnmounted} from 'vue';
+import {invoke} from "@tauri-apps/api/tauri";
+import {listen} from "@tauri-apps/api/event";
+import * as echarts from 'echarts';
+import {EChartsType} from "echarts";
+
+let myChart: undefined | EChartsType = undefined;
+const cpuJson = ref("");
+const processJson = ref("");
+
+let listen_list: any[] = [];
+let cpu_msg_list: any[] = [];
+let cpu_msg_list_v: any[] = [];
+let process_msg_list: any[] = [];
+
+const initListen = () => {
+  invoke('init_process');
+
+  let un_listen = listen('on_cpu_msg', (event: any) => {
+    console.log("wtf0: ", event)
+    const json = event.payload.toString();
+    cpuJson.value = json;
+    console.log("wtf1: ", json)
+    const jsonObject = JSON.parse(json);
+    console.log("wtf2: ", jsonObject)
+    cpu_msg_list.push(jsonObject);
+    cpu_msg_list_v.push(jsonObject["ave"]["usage"]);
+    myChart?.setOption({
+      series: [
+        {
+          data: cpu_msg_list_v,
+        }
+      ]
+    });
+  });
+  listen_list.push(un_listen);
+
+  un_listen = listen('on_process_msg', (event: any) => {
+    const json = event.payload.toString();
+    processJson.value = json;
+    const jsonObject = JSON.parse(json);
+    process_msg_list.push(jsonObject);
+  });
+  listen_list.push(un_listen);
+}
+
+const initCharts = () => {
+  const chartDom = document.getElementById('chart');
+  myChart = echarts.init(chartDom);
+  let option = {
+    xAxis: {
+      type: 'category'
+    },
+    yAxis: {
+      type: 'value'
+    },
+    series: [
+      {
+        data: cpu_msg_list_v,
+        type: 'line',
+        smooth: true
+      }
+    ]
+  };
+
+  myChart.setOption(option);
+  window.onresize = () => {
+    myChart?.resize()
+  };
+}
+
+onMounted(() => {
+  initListen();
+  initCharts();
+});
+
+onUnmounted(() => {
+  listen_list.map((cb: any) => {
+    cb();
+  })
+});
 </script>
 
 <template>
-  <div class="container">
-    <h1>Welcome to Tauri!</h1>
-
-    <div class="row">
-      <a href="https://vitejs.dev" target="_blank">
-        <img src="/vite.svg" class="logo vite" alt="Vite logo" />
-      </a>
-      <a href="https://tauri.app" target="_blank">
-        <img src="/tauri.svg" class="logo tauri" alt="Tauri logo" />
-      </a>
-      <a href="https://vuejs.org/" target="_blank">
-        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-      </a>
-    </div>
-
-    <p>Click on the Tauri, Vite, and Vue logos to learn more.</p>
-
-    <p>
-      Recommended IDE setup:
-      <a href="https://code.visualstudio.com/" target="_blank">VS Code</a>
-      +
-      <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-      +
-      <a href="https://github.com/tauri-apps/tauri-vscode" target="_blank"
-        >Tauri</a
-      >
-      +
-      <a href="https://github.com/rust-lang/rust-analyzer" target="_blank"
-        >rust-analyzer</a
-      >
-    </p>
-
-    <Greet />
-  </div>
+  <div id="chart" style="display: flex; height: 300px"></div>
+  <p>on_cpu_msg: {{ cpuJson }}</p>
+  <p>on_process_msg: {{ processJson }}</p>
 </template>
-
-<style scoped>
-.logo.vite:hover {
-  filter: drop-shadow(0 0 2em #747bff);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #249b73);
-}
-</style>
