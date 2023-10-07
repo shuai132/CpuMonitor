b56ab2582ba0cfa560bb9dea83ea8d6bfb6ab9a4
liushuai <770722922@qq.com>
2023-10-07 18:38:50 +0800

    fix: timestamps error

--- daemon/utils/time_utils.h
+++ daemon/utils/time_utils.h
@@ -5,7 +5,7 @@
 namespace utils {
 
 uint64_t getTimestamps() {
-  auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now().time_since_epoch());
+  auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch());
   return ms.count();
 }
 
